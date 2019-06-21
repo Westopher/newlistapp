39452e82a64ab5f36ef0f8af01fab6f010174d26
West Kraemer <kraemerwest@gmail.com>
2019-06-21 16:28:29 +0200

    first commit in a while. was reviewing with 100 days of swift

---
+++ newListApp/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  newListApp
+//
+//  Created by West Kraemer on 6/21/19.
+//  Copyright © 2019 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var pictures = [String]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
+        
+    }
+
+
+}
+
